811c6a2fafbf44350fe9058d4812c03f17ee222b
asd12377952 <asd12377952@yahoo.com>
2022-09-18 00:36:03 +0800

    first

---
+++ 程式/boxmove.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class boxmove : MonoBehaviour
+{
+	//子彈的飛行時間為一秒，如果時間太長或不消失的話子彈會一直線的飛行下去
+	public float boxLife = 0.6f;
+	//計數器與變量
+	private float lifeCounter = 0.0f;
+
+	
+	void Update () {
+		//加上計數器的變量
+		lifeCounter += Time.deltaTime;
+		
+		//如果計數器大於設定的秒速(目前設定為1秒)就要毀掉這個物件
+		if(lifeCounter > boxLife){
+			Destroy(gameObject);
+		}
+	}
+
+	//攻擊標記tag為Player的玩家並且摧毀物件
+	void OnTriggerEnter (Collider other){
+		if(other.tag == "box"){
+			Destroy(gameObject);
+		}
+	}
+}
+
