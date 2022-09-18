a25676a61e7ee6dffeb7da277e39f961ef309f91
asd12377952 <asd12377952@yahoo.com>
2022-09-18 20:36:03 +0800

    first

--- 程式/fireBall.cs
+++ 程式/fireBall.cs
@@ -4,15 +4,25 @@ using UnityEngine;
 
 public class fireBall : MonoBehaviour
 {
+    public float range;
+    public float speed;
+    public int damage;
+    private Vector3 Pos;
     // Start is called before the first frame update
     void Start()
     {
-        
+        Pos = gameObject.transform.position;
+        gameObject.transform.position += new Vector3(0, range, 0);
     }
 
     // Update is called once per frame
     void Update()
     {
-        transform.RotateAround(new Vector3(0, 0, 0), new Vector3(0, 0, 1), 360 * Time.deltaTime *2);
+        transform.RotateAround(Pos, new Vector3(0, 0, 1), 360 * Time.deltaTime * speed);
+    }
+    void OnTriggerEnter (Collider other){
+		if(other.tag == "Player"){
+			other.SendMessage("takeDamage", damage, SendMessageOptions.DontRequireReceiver);
+		}
     }
 }
